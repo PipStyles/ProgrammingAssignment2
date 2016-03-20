2689f0d6fc82be4e57f40750a74485af9c979b96
Philip Styles <philipstyles77@gmail.com>
2016-03-20 13:01:35 +0000

    functions tested and working

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions to a) cache a matrix and its inverse
+##  and b) calculate and set the inverse of a matrix in the cache function
 
-## Write a short comment describing this function
+## pipstyles: I don't really like this configuration of functions. 
+## makeCacheMatrix is 'confused' in naming/purpose
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  
+  inv <- NULL
+  
+  setMatrix <- function(m) {
+    mat <<- m
+    inv <<- NULL
+  }
+  
+  getMatrix <- function() {
+    mat
+  }
+  
+  ##poor design decisions IMO - this function could just be a generic cache
+  ## and not concern itself with specific methods? Or it could just perform
+  ## the solving and caching itself (but that would break single responsibility
+  ## so, perhaps not)
+  
+  setInverse <- function(i) {
+    inv <<- i
+  }
+  
+  getInverse <- function() {
+    inv
+  }
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInverse = setInverse, getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## Checks if "special" matrix (x) cache function's inverse is NULL, solves if required and 
+## sets in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Check for null...
+  if(is.null(x$getInverse())) {
+    message("setting cached matrix")
+    x$setInverse(solve(x$getMatrix(), ...))
+  }
+  x$getInverse()
 }
